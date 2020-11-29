44398a6865d6fb71b8f7cff07e475e39fa6acaf8
Fedir <zi.12feodor@gmail.com>
2020-11-29 22:54:08 +0200

    done 01

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
 import './App.css';
+import Profile from './components/Profile';
+import Statistics from './components/Statistics';
+import FriendList from './components/FriendList';
+import TransactionHistory from './components/Transactions';
 
-function App() {}
+import statisticalData from './json/statistical-data.json';
+import user from './json/user.json';
+import friends from './json/friends.json';
+import transactions from './json/transactions.json';
+
+function App() {
+  return (
+    <main>
+      <h1>Tasks</h1>
+      <h2>Task 01</h2>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <h2>Task 2</h2>
+      <Statistics title="UPLOADS STATS" stats={statisticalData} />
+      <p>-</p>
+      <Statistics stats={statisticalData} />
+      <h2>Task 03</h2>
+      <FriendList friends={friends} />
+      <h2>Task 04</h2>
+      <TransactionHistory items={transactions} />
+    </main>
+  );
+}
 
 export default App;
